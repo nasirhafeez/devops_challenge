7728418ebdf1ea506285f58b65ade82fc26488e0
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-05-01 11:17:33 +0500

    correction in startup script; updated readme

--- my_share/startup.sh
+++ my_share/startup.sh
@@ -4,4 +4,4 @@ apk add --update python3
 apk add py3-pip gcc musl-dev mariadb-connector-c-dev python3-dev mariadb-dev
 pip install flask mysqlclient
 
-python3 /test-pd/python_script.py
+python3 /test-pd/hello.py
